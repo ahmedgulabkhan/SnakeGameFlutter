022a9ca3069d7b62c5c0a9f74bce171b1f2e8b6a
demoncommand97 <demoncommand97@gmail.com>
2020-09-05 22:12:16 +0530

    :tada: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:SnakeGameFlutter/game_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SnakeGameFlutter',
+      debugShowCheckedModeBanner: false,
+      // home: GameWidget(),
+    );
+  }
+}
