feee168e9bbf7a560aa0c6d5b3f2e5d722f7a69f
demoncommand97 <demoncommand97@gmail.com>
2020-09-06 17:10:08 +0530

    :zap: changes to game-over page

--- lib/game_over.dart
+++ lib/game_over.dart
@@ -1,3 +1,4 @@
+import 'package:SnakeGameFlutter/game_page.dart';
 import 'package:flutter/material.dart';
 
 class GameOver extends StatelessWidget {
@@ -11,9 +12,34 @@ class GameOver extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Game over, score: $score'),
-      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.blue,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Game Over', style: TextStyle(color: Colors.redAccent, fontSize: 24.0, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic)),
+            
+            SizedBox(height: 50.0),
+
+            Text('Your Score is: $score', style: TextStyle(color: Colors.white, fontSize: 20.0)),
+
+            SizedBox(height: 50.0),
+
+            FlatButton.icon(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+              color: Colors.redAccent,
+              onPressed: () {
+                Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => GamePage()));
+              },
+              icon: Icon(Icons.refresh, color: Colors.white, size: 30.0),
+              label: Text("Try Again", style: TextStyle(color: Colors.white, fontSize: 20.0))
+            ),
+          ],
+        ),
+      )
     );
   }
 }
